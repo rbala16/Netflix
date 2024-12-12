2e2ac1cb97971c6b5138e2d8c2cf1cbfa23f4a69
Rajni <balatamna16@gmail.com>
2024-12-12 18:49:07 -0500

    tailwindcss installation

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const LOGO_IMG = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7b21-92dd-d4d4b93ad8a6/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const LOGIN_BACK_IMG="https://assets.nflxext.com/ffe/siteui/vlv3/729ce5c2-d831-436a-8c9d-f38fea0b99b3/web/CA-en-20241209-TRIFECTA-perspective_7c8dee77-e6ce-46ab-8282-c1a771cb5aad_small.jpg";
