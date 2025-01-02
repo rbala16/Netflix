0df6a40c600a060447d70d8cbbf95fe68e2a8995
Rajni <balatamna16@gmail.com>
2025-01-02 13:38:57 -0500

    created custom hook for HeroTrailer fetching

--- src/hooks/useHeroTrailer.js
+++ src/hooks/useHeroTrailer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { API_OPTIONS } from '../utils/constants'
+import { addTrailerVideos } from '../features/heroSlice'
 
-const useHeroTrailer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const useHeroTrailer = (movieId) => {
+  const dispatch = useDispatch()
+  const getMovieIds = async ()=>{
+    const url = 'https://api.themoviedb.org/3/movie/' + movieId + '/videos?language=en-US';
+    const backgroundData = await fetch(url,API_OPTIONS)
+    const trailers = await backgroundData.json()
+    dispatch(addTrailerVideos(trailers?.results[0]))
+  }
+ useEffect(()=>{
+  getMovieIds()
+ },[])
 }
 
 export default useHeroTrailer
