c254b5aad3defad2eab79ad20d0fb3dfcfdb3cc1
Rajni <balatamna16@gmail.com>
2025-01-01 19:40:34 -0500

    setup heroSlice for trailer video

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../features/userSlice";
 import movieSlice from "../features/movieSlice";
-
+import heroSlice from "../features/heroSlice";
 const appStore = configureStore({
     reducer: {
         user: userSlice,
-        movie:movieSlice
+        movie:movieSlice,
+        hero:heroSlice,
     }
 })
 
