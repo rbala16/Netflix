32fd2e5316dc047d8f7c6c3508865d8bfd1a82b6
Rajni <balatamna16@gmail.com>
2025-05-02 16:42:39 -0400

    .env updated

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwHWPFpwEp1oCPtOiBE8bnZCMAruWd7Ss",
-  authDomain: "netflix-3f606.firebaseapp.com",
-  projectId: "netflix-3f606",
-  storageBucket: "netflix-3f606.firebasestorage.app",
-  messagingSenderId: "2723013447",
-  appId: "1:2723013447:web:6dedee43f675ed2ca1a262",
-  measurementId: "G-H02JF3XEFD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
